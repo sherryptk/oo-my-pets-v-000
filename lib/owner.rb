721a3ad228b0a466c6cfa8f9bfb3e17b76c63560
Sherry El Garhy <sherryptk@gmail.com>
2017-11-21 01:39:50 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,85 @@
+require 'pry'
+
 class Owner
   # code goes here
+  attr_accessor :name, :buy_fish, :pets
+  attr_reader :species, :say_species
+
+  @@all =[]
+
+  def initialize(name = nil)
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all << self
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def species
+    @species = "human"
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    @pets.each do |k,v|
+      v.each do |v|
+        v.mood="happy"
+      end
+    end
+  end
+
+  def play_with_cats
+    @pets.each do |k,v|
+      v.each do |v|
+        v.mood="happy"
+      end
+    end
+  end
+
+  def feed_fish
+    @pets.each do |k,v|
+      v.each do |v|
+        v.mood="happy"
+      end
+    end
+  end
+
+  def sell_pets
+    @pets.each do |k,v|
+      v.each do |v|
+        v.mood = "nervous"
+      end
+    end
+    @pets.clear
+  end
+
+def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+  end
+
 end
